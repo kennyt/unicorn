6e39849127a770f7607530003615fa8327eb39c4
Kenny Tran <kenny8tran@gmail.com>
2015-04-27 10:40:55 -0700

    Basic client side set up

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3050, function() {
+app.listen(3000, function() {
   console.log('Listening on port: ' + 3000);
 });
