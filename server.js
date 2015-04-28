3a56387a47bd27c29e742734bb31ffd18090fba1
Kenny Tran <kenny8tran@gmail.com>
2015-04-28 10:39:16 -0700

    Added process.env.PORT to server.js for deployment

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function() {
-  console.log('Listening on port: ' + 3000);
+app.listen(port, function() {
+  console.log('Listening on port: ' + port);
 });
